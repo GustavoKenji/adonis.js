faa9ff2dcadd47dfee33b9f4fe0ec3ef967d531d
Gustavo Kenji <gustavokomatsu4@gmail.com>
2020-07-19 18:01:56 -0300

    coupon user relationship

--- database/migrations/1595192163262_coupon_user_schema.js
+++ database/migrations/1595192163262_coupon_user_schema.js
@@ -5,14 +5,28 @@ const Schema = use('Schema')
 
 class CouponUserSchema extends Schema {
   up () {
-    this.create('coupon_users', (table) => {
+    this.create('coupon_user', (table) => {
       table.increments()
+      table.integer('coupon_id').unsigned()
+      table.integer('user_id').unsigned()
       table.timestamps()
+
+      table
+        .foreign('coupon_id')
+        .references('id')
+        .inTable('coupons')
+        .onDelete('cascade')
+
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('cascade')
     })
   }
 
   down () {
-    this.drop('coupon_users')
+    this.drop('coupon_user')
   }
 }
 
