6abbbfecc3987823a78d1d31bcb13c78a4853b20
Gustavo Kenji <gustavokomatsu4@gmail.com>
2020-07-20 11:45:11 -0300

    category, product and coupon models

---
+++ app/Models/Category.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Category extends Model {
+  /**
+   * Relacionamento entre Categoria e Imagem de destaque
+   */
+  image() {
+    return this.belongsTo('App/Models/Image')
+  }
+
+  /**
+   * Relacionamento entre Categoria e Produtos
+   */
+  products() {
+    return this.belongsToMany('App/Model/Product')
+  }
+}
+
+module.exports = Category
---
+++ app/Models/Product.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Product extends Model {
+  image() {
+    return this.belongsTo('App/Models/Image')
+  }
+
+  /**
+   * Relacionamento entre produto e imagens
+   * Galeria de imagens do produto
+   */
+  images() {
+    return this.belongsToMany('App/Models/Image')
+  }
+
+  /**
+   * Relacionamento entre produtos e categorias
+   */
+  products() {
+    return this.belongsToMany('App/Models/Product')
+  }
+
+  /**
+   * Relacionamento entre produtos e cupons de desconto
+   */
+  coupons() {
+    return this.belongsToMany('App/Models/Coupon')
+  }
+}
+
+module.exports = Product
